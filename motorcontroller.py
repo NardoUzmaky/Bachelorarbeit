f201c071cd9beb97f8b39943829c1b0d91300d66
Simon Frölich <simon.froelich@greenmail.ch>
2023-12-15 21:24:19 +0100

    improved motor controller by adding stopper

--- motorcontroller.py
+++ motorcontroller.py
@@ -1,34 +1,49 @@
 import RPi.GPIO as gpio
 import time
+from potentiometer import read_potentiometer
  
 def init():
  gpio.setmode(gpio.BCM)
  gpio.setup(23, gpio.OUT)
  gpio.setup(22, gpio.OUT)
- gpio.setup(27, gpio.OUT)
+ gpio.setup(24, gpio.OUT)
  
-def forward(sec):
+def turn_motor(sec, direction, speed):
 	init()
-	gpio.output(23, True) #In1
-	gpio.output(22, False)#In2
-	pwm = gpio.PWM(27, 1000) #EN
+	if speed > 30:
+		print("Too fast")
+		return
+	
+	if direction == "forward":
+		print("forward")
+		gpio.output(23, True) #In1
+		gpio.output(22, False)#In2
+	elif direction == "reverse":
+		print("reverse")
+		gpio.output(23, False) #In1
+		gpio.output(22, True)#In2
+	else:
+		print("Enter valid Direction")
+		return
+	pwm = gpio.PWM(24, 1000) #EN
 	pwm.start(0)
-	pwm.ChangeDutyCycle(100)
-	time.sleep(sec)
- 
-def reverse(sec):
- init()
- gpio.output(23, False)
- gpio.output(22, True)
- #gpio.output(23, False) 
- #gpio.output(24, True)
- time.sleep(sec)
+	pwm.ChangeDutyCycle(speed)
+	interval = 0.1 # measurement interval
+	for i in range(int(sec/interval)):
+		val = read_potentiometer()
+		if direction == "forward" and val < 1.1:
+			print("stopper activated")
+			break
+		if direction == "reverse" and val > 2:
+			print("stopper activated")
+			break
+		time.sleep(interval)
+	pwm.ChangeDutyCycle(0)
+
  
 try: 
-	print("forward")
-	forward(100)
-	print("reverse")
-	reverse(2)
+	turn_motor(5, "forward", 3)
+	turn_motor(5, "reverse", 3)
 except KeyboardInterrupt:
 	print("Keyboard interrupt")
 except Exception as e:
--- potentiometer.py
+++ potentiometer.py
@@ -4,10 +4,11 @@ import board
 import adafruit_ads1x15.ads1115 as ADS
 from adafruit_ads1x15.analog_in import AnalogIn
 
-i2c = busio.I2C(board.SCL, board.SDA)
+def read_potentiometer():
+	i2c = busio.I2C(board.SCL, board.SDA)
+	ads = ADS.ADS1115(i2c)
+	chan = AnalogIn(ads, ADS.P0)
+	#print(chan.value, chan.voltage)
+	return chan.voltage
 
-ads = ADS.ADS1115(i2c)
-
-chan = AnalogIn(ads, ADS.P0)
-
-print(chan.value, chan.voltage)
+read_potentiometer()
